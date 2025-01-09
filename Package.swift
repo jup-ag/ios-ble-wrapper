141fcbf8928c6dde6e282644f6736e8876e9eefb
Junda <junda@raccoons.dev>
2025-01-09 12:59:23 +0800

    use updated BleTransport from tonkeeper

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["BleWrapper"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/LedgerHQ/hw-transport-ios-ble", branch: "main")
+        .package(url: "https://github.com/tonkeeper/hw-transport-ios-ble", branch: "main")
     ],
     targets: [
         .target(
